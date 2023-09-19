f50d70faff6a541f1959f60f5fe8b555b3ab029d
new name <email for my second account>
2023-09-20 01:56:16 +0600

    first commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(
 );
 app.use(
   cors({
-    origin: ["https://master--dev-community-mern.netlify.app/"],
+    origin: ["https://dev-community-mern.netlify.app/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: [
